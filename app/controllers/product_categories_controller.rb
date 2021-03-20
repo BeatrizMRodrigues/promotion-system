e375321f0ade88fab7aff726a67d4cec991c43f9
beatrizmntr <anabeatrizmonteirorodrigues@gmail.com>
2021-03-20 17:15:32 -0300

    Criando categorias

--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -2,4 +2,21 @@ class ProductCategoriesController < ApplicationController
     def index
         @product_categories = ProductCategory.all
     end
+
+    def new
+        @product_category = ProductCategory.new
+    end
+
+    def show
+        @product_category = ProductCategory.find(params[:id])
+    end
+
+    def create
+        @product_category = ProductCategory.new(params.require(:product_category).permit(:name, :code))
+        if @product_category.save
+            redirect_to @product_category
+        else
+            render :new
+        end
+    end
 end
--- test/system/product_categories_test.rb
+++ test/system/product_categories_test.rb
@@ -12,4 +12,36 @@ class ProductCategoriesTest < ApplicationSystemTestCase
      assert_text 'Produto AntiFraude'
      assert_text 'ANTIFRA'
     end
+
+    test 'no category are available' do
+     visit root_path
+     click_on 'Categorias'
+
+     assert_text 'Nenhuma categoria cadastrada'
+    end
+
+    test 'view category and return to home page' do 
+     ProductCategory.create!(name: 'Produto AntiFraude', 
+                            code: 'ANTIFRA')
+    
+     visit root_path
+     click_on 'Categorias'
+     click_on 'Voltar'
+
+     assert_current_path root_path
+    end
+
+    test 'create category' do 
+     visit root_path
+     click_on 'Categorias'
+     click_on 'Cadastrar nova categoria'
+     fill_in 'Nome', with: 'Produto AntiFraude'
+     fill_in 'Código', with: 'ANTIFRA'
+     click_on 'Cadastrar'
+
+     assert_current_path product_category_path(ProductCategory.last)
+     assert_text 'Produto AntiFraude'
+     assert_text 'ANTIFRA'
+     assert_link 'Voltar'
+    end
 end
