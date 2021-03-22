0dbd5c8390fb428d40bc2de71faea1965a58acdd
beatrizmntr <anabeatrizmonteirorodrigues@gmail.com>
2021-03-22 16:03:56 -0300

    Testes unitários para gerar cupons

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -5,4 +5,11 @@ class Promotion < ApplicationRecord
               :expiration_date, presence: {message: 'não pode ficar em branco'}
 
     validates :name, :code, uniqueness: {message: 'deve ser único'}
+
+    def generate_coupons!
+     return unless coupons.empty?
+        (1..coupon_quantity).each do |number|
+            coupons.create!(code: "#{code}-#{'%04d' % number}")
+        end
+    end
 end
